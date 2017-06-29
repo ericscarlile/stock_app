711c8b8ced8600b68bb41b0948281588e6de87e2
Eric <ericscarlile@gmail.com>
2017-06-29 15:09:08 -0500

    Create StockSearchView to connect to Yahoo Finance API to resolve company names to stock symbols. WIP: StockSearchView only grabs the JSONP, formats it and redirects to the URL grabbed from. Still need to parse the results and print them.

--- stock_app/views.py
+++ stock_app/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+
+import urllib.request
+import json
 
 
 class IndexView(LoginRequiredMixin, generic.ListView):
@@ -13,3 +16,18 @@ class IndexView(LoginRequiredMixin, generic.ListView):
         template_name = 'stock_app/index.html'
         return render(request, template_name)
 
+
+class StockSearchView(LoginRequiredMixin, generic.ListView):
+
+    def post(self, request, *args, **kwargs):
+        if request.POST['search_key']:
+            search_key = request.POST['search_key']
+            search_url = 'http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en&callback=YAHOO.Finance.SymbolSuggest.ssCallback'.format(search_key)
+
+            # The search_url will return a JSONP not a JSON.
+            # This code formats the JSONP to a JSON so it can be used.
+            response = urllib.request.urlopen(search_url).read().decode('utf8')
+            data_json = response.split("(")[1].split(")")[0]
+            obj = json.loads(data_json)
+
+            return redirect(search_url)
